1642a5c75f95d521cf92e74b55859bbc302f4e42
omar11taha <omartawaha95@gmail.com>
2021-09-18 01:53:09 +0300

    add hedar and footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,44 @@
-import React, { Component } from 'react'
+import React from "react";
+import { Navbar, Container, Nav ,NavDropdown } from "react-bootstrap";
+// import { Link } from "react-router-dom";
+import "../Style/header.css";
+// import NavLink from "react-bootstrap/NavLink";
+// import LoginButton from "./LoginButton";
+// import LogoutButton from "./LogoutButton";
+// import { withAuth0 } from "@auth0/auth0-react";
 
-export class Header extends Component {
-    render() {
-        return (
-            <div>
+class Header extends React.Component {
+  render() {
+    // const isAuth = this.props.auth0.isAuthenticated;
+    return (
+      <>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">Home</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#Collection" style={{paddingLeft:"250px"}}>Collection</Nav.Link>
+              <Nav.Link href="#Event" style={{paddingLeft:"50px"}}>Event</Nav.Link>
+              <NavDropdown title="Profile" id="collasible-nav-dropdown" style={{paddingLeft:"50px"}}>
+                <NavDropdown.Item href="#logIn" >log in</NavDropdown.Item>
+                <NavDropdown.Item href="#logOut">
+                  log out
+                </NavDropdown.Item>
                 
-            </div>
-        )
-    }
+              </NavDropdown>
+              <NavDropdown title="About us" id="collasible-nav-dropdown" style={{paddingLeft:"50px"}}>
+                <NavDropdown.Item href="#about">About team</NavDropdown.Item>
+                <NavDropdown.Item href="#feedback">
+                  feedback
+                </NavDropdown.Item>
+                
+              </NavDropdown>
+            </Nav>
+          </Container>
+        </Navbar>
+        <br />
+      </>
+    );
+  }
 }
 
-export default Header
+export default Header;
