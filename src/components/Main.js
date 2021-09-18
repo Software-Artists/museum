6d957beff82d5309b8b02a20f3556bd3cf73cc18
issasalman <issasalmanhu1996@gmail.com>
2021-09-18 20:06:56 +0300

    home page rendering

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,86 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import axios from "axios";
+import Carousel from "react-bootstrap/Carousel";
+import Row from "react-bootstrap/Row";
+import Card from "react-bootstrap/Card";
 
 export class Main extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      museumData: [],
+      paintingsData: [],
+    };
+  }
+
+  componentDidMount = () => {
+    axios
+      .get(`${process.env.REACT_APP_SERVER_URL}/museum`)
+      .then((museumResponse) => {
+        this.setState({ museumData: museumResponse.data });
+      });
+    axios
+      .get(`${process.env.REACT_APP_SERVER_URL}/paintings`)
+      .then((paintingsResponse) => {
+        this.setState({ paintingsData: paintingsResponse.data });
+      });
+  };
+
+  render() {
+    console.log(this.state.museumData);
+    return (
+      <>
+        <div class="d-flex justify-content-center">
+          <Carousel fade className="d-block w-75">
+            {this.state.museumData.map((element) => {
+              return (
+                <Carousel.Item interval={1500}>
+                  <img
+                    className="d-block w-100"
+                    src={element.museum_image}
+                    alt="Image One"
+                  />
+                  <Carousel.Caption>
+                    <h3>{element.name}</h3>
+                    <p>{element.location}</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              );
+            })}
+          </Carousel>
+        </div>
+        <div>
+          <Row xs={1} md={4} className="g-4">
+            {this.state.paintingsData.splice(0,2).map((element) => {
+              return (
+                <Card
+                  style={{
+                    width: "20rem",
+                    border: "solid",
+                    margin: "40px",
+                  }}
+                >
+                  <Card.Body
+                    style={{
+                      color: "black",
+                      
+                    }}
+                  >
+                    <Card.Img
+                      variant="top"
+                      src={element.art_image1}
+                      alt={element.description_image1}
+                    />
+                    <Card.Title>{element.description_image1}</Card.Title>
+                  </Card.Body>
+                </Card>
+              );
+            })}
+          </Row>
+        </div>
+      </>
+    );
+  }
 }
 
-export default Main
+export default Main;
