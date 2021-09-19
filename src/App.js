8d752dd119615826f2b29f924fe05ca5b05c308d
NashatAlzaatreh <nashat.alzaatra@yahoo.com>
2021-09-19 11:20:21 +0300

    make collection page form and modal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import "./App.css";
 import React from "react";
-import Collections from "./components/Collections";
 class App extends React.Component {
   render() {
-    return (
-      <div id="bodyDiv">
-        <Collections />
-      </div>
-    );
+    return <div id="bodyDiv"></div>;
   }
 }
 
