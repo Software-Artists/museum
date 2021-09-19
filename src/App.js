ddebc9c7cf4e9e1c2b388424f733d20f1aa6abe5
hebah al daealieb <hebah20082gmail.com>
2021-09-19 15:48:34 +0300

    about us

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import React from "react";
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Aboutus from "./components/Aboutus";
 
 function App() {
   return (
     <div className="App">
       
       <Header/>
+      <Aboutus/>
       <Footer/>
+      
     </div>
   );
 }
