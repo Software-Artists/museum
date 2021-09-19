5c8e586e6aabb4841ff7387479a5663770d8e9b2
aajaber <aajaber.97@gmail.com>
2021-09-19 16:02:28 +0300

    front end feedback

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 // import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-
+import "./App.css";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+import FeedBack from "./components/Feedback";
 function App() {
   return (
     <div className="App">
-      
-      <Header/>
-      <Footer/>
+      {/* <Header/>
+      <Footer/> */}
+      <FeedBack />
     </div>
   );
 }
